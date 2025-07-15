18ab2044a797c5cdd28adbf9bf47dbef41067795
youssefhossam29 <yh5719731@gmail.com>
2025-07-16 01:01:10 +0300

    Fix PatientResource: format date_of_birth using Carbon to avoid null response

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -19,7 +19,9 @@ class PatientResource extends JsonResource
             'id'    => $this->id,
             'user'  => new UserResource($this->user),
             'image' => 'uploads/users/' . $this->image,
-            'date_of_birth' => optional($this->date_of_birth)->toDateTimeString(),
+            'date_of_birth' => $this->date_of_birth
+                ? \Carbon\Carbon::parse($this->date_of_birth)->format('Y-m-d')
+                : null,
             'gender' => $this->gender_type,
             'phone' => $this->phone,
         ];
