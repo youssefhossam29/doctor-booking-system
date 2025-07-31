5bbc231d8c9837896427b5dcf7d5fbd1b83f6231
youssefhossam29 <yh5719731@gmail.com>
2025-07-31 15:29:34 +0300

    Create contacts and contact_replies migration files to support doctor/patient-to-admin communication

--- database/migrations/2025_07_31_084437_create_contact_replies_table.php
+++ database/migrations/2025_07_31_084437_create_contact_replies_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contact_replies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->tinyInteger('type')->default(1);
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('contact_id')->constrained('contacts')->onDelete('cascade');
+            $table->tinyInteger('user_type');
+            $table->text('message');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contact_replies');
     }
 };
